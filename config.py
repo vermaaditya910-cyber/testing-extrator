c54b3660cb3052f21148472d96735ab05043d608
vermaaditya910-cyber <vermaaditya910@gmail.com>
2026-01-20 19:43:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+class Config(object):
+    # Telegram Bot ka token
+    BOT_TOKEN = "7896379697:AAGX__ZjhrbBSdjeUIu6crWkmI6GgWCekbo"
+    # Telegram API ki ID
+    API_ID = 27030443
+    API_HASH = "da842bsdfijfhbeuf39edf0ac74b6e00c43c91"
+    ADMIN_ID = [6697397532]
+    # MongoDB database ka URL
+    DB_URL = "mongodb+srv://username:password@dddd.octnw1p.mongodb.net/"
+    # Database ka naam
+    DB_NAME = "MY_BOT_DB"
+    # Text log channel ki ID
+    TXT_LOG = -1002667681315
+    # Authentication log channel ki ID
+    AUTH_LOG = -1002472481034
+    # Hit log channel ki ID
+    HIT_LOG = -1002472481034
+    # DRM dump channel ki ID
+    DRM_DUMP = -1002472481034
+    # Main channel ki ID
+    CHANNEL = -1002361507681
+    # Channel ka link
+    CH_URL = "https://t.me/+VbMs1OavyItjNjFl"
+    # Bot ke owner ka Telegram link
+    OWNER = "https://t.me/jattprab"
+    # Thumbnail image ka URL
+    THUMB_URL = "https://telegra.ph/file/example-thumb-image.jpg" #Replace by with your Thumb URL
